5108700d1908f29da2531a4ab868360cdf58a47f
Niek Geerligs <geerligsniek@gmail.com>
2026-03-20 09:05:52 +0100

    initial commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,96 @@
+import 'package:cluck_catch/pages/game_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Positioned.fill(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: Get.height * .5,
+              child: Container(color: Color(0xff5EB5B9)),
+            ),
+            Positioned.fill(
+              child: Transform.scale(
+                scale: 3,
+                child: Image.asset("assets/images/sun.png"),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: Get.height * .5,
+              bottom: 0,
+              child: Container(color: Color(0xff6E8C39)),
+            ),
+            Positioned.fill(
+              child: Transform.translate(
+                offset: Offset(0, 100),
+                child: Transform.scale(
+                  scale: 1.5,
+                  child: Image.asset("assets/images/grass.png"),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/images/logo_cluck_and_catch.png",
+                    width: Get.width * .3,
+                  ),
+                  Image.asset(
+                    "assets/images/chicken_coop.png",
+                    width: Get.width * .3,
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: Get.width * .3,
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xffF47E23),
+                        ),
+                        foregroundColor: WidgetStatePropertyAll(
+                          Color(0xffffffff),
+                        ),
+                      ),
+                      onPressed: () => GamePage(),
+                      child: Text("START"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                    width: Get.width * .3,
+                    child: TextButton(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStatePropertyAll(
+                          Color(0xffffffff),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text("SCORES"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
